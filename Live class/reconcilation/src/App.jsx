c976e1d62d497c4207e0b3353f7cd13f2feb86bb
GyaneshwerJha <keshavk72411@gmail.com>
2024-01-19 10:06:36 +0530

    useEffect, useCallback, useMemo ✅

--- Live class/reconcilation/src/App.jsx
+++ Live class/reconcilation/src/App.jsx
@@ -1,14 +1,44 @@
-import React, { useState } from 'react'
+import React, { useCallback, useEffect, useState, memo } from "react";
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const [balance1, setBalance1] = useState({ returns: 0 });
+  const [balance2, setBalance2] = useState({ returns: 0 });
+  const [incomes, setIncome] = useState({ income: 0 });
+
+  useEffect(() => {
+    setBalance1({
+      returns: 100,
+    });
+  }, []);
+
+  useEffect(() => {
+    setBalance2({
+      returns: 100,
+    });
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIncome({
+        income: 100,
+      });
+    }, 5000);
+  }, []);
+
+  const calculateBalance = useCallback(() => {
+    return balance1.returns + balance2.returns;
+  }, [balance1, balance2]);
+
   return (
     <div>
-      <button onClick={() => {
-        setCount(count + 1)
-      }}>count is {count}</button>
+      <BalanceCalculator calculateBalance={calculateBalance}></BalanceCalculator>
     </div>
-  )
-}
+  );
+};
 
-export default App
+const BalanceCalculator = memo(({ calculateBalance }) => {
+  console.log("balance child re-render");
+  return <div>Your balance is {calculateBalance()}</div>;
+});
+
+export default App;
